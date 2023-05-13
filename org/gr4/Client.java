e10900795523465785ab6fe82f970489de91fb71
Leutrim Hajdini <leutrimhajdini63@gmail.com>
2023-05-14 00:02:19 +0200

    Testing the program to see if it works.

--- org/gr4/Client.java
+++ org/gr4/Client.java
@@ -1,4 +1,16 @@
 package org.gr4;
 
 public class Client {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+
+        int number = 18;
+        System.out.println("The number is: " + number);
+
+        // Print multiple values
+        String name = "Grupi 4";
+        int age = 22;
+        System.out.println("Name: " + name + ", Age: " + age);
+    }
 }
+
