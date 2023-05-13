241c9c8b0c667d36b8bf80402778deac68f167d9
Leutrim Hajdini <leutrimhajdini63@gmail.com>
2023-05-14 00:02:46 +0200

    ...

--- org/gr4/Client.java
+++ org/gr4/Client.java
@@ -4,13 +4,7 @@ public class Client {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
 
-        int number = 18;
-        System.out.println("The number is: " + number);
 
-        // Print multiple values
-        String name = "Grupi 4";
-        int age = 22;
-        System.out.println("Name: " + name + ", Age: " + age);
     }
 }
 
