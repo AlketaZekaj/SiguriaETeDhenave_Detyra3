6db37385d5f223f73a230fc7dc4dcd6c7fe3a033
Alketa Zekaj <118227007+AlketaZekaj@users.noreply.github.com>
2023-05-14 14:21:52 +0200

    .

--- org/gr4/Client.java
+++ org/gr4/Client.java
@@ -1,6 +1,19 @@
 package org.gr4;
 
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.GCMParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+import java.util.Base64;
+
+/**
+ * Possible KEY_SIZE values are 128, 192 and 256
+ * Possible T_LEN values are 128, 120, 112, 104 and 96
+ */
+
 public class Client {
+
     public static void main(String[] args) {
         System.out.println("Hello, World!");
 
