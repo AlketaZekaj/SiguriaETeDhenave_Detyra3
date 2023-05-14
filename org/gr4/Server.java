c364e9ea3911c0ac83b446b260923021290e1e5f
Genti Krasniqi <59203667+gentikk@users.noreply.github.com>
2023-05-14 19:21:34 +0200

    Update Server.java

--- org/gr4/Server.java
+++ org/gr4/Server.java
@@ -2,6 +2,8 @@ package org.gr4;
 
 import javax.crypto.Cipher;
 import javax.crypto.KeyGenerator;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.GCMParameterSpec;
 
 public class Server {
     public static void main(String[] args) {
